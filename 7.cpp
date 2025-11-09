e130957162b5634d5550e09318944185eddd009f
SAMEER ALI <cu25260290@coeruniversity.ac.in>
2025-11-09 22:52:21 +0530

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[2][2], b[2][2], sum[2][2];
+    cout << "Enter 2x2 matrix A:\n";
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            cin >> a[i][j];
+    cout << "Enter 2x2 matrix B:\n";
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            cin >> b[i][j];
+    cout << "Sum of matrices:\n";
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
