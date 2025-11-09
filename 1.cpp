adaf0e1447c412a062fe3125b7ca46fd834edb89
SAMEER ALI <cu25260290@coeruniversity.ac.in>
2025-11-09 22:39:12 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= n / 2; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (isPrime(n))
+        cout << n << " is Prime";
+    else
+        cout << n << " is Not Prime";
+    return 0;
+}
